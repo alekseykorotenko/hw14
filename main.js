c1c972332cf86719b190574d3b5cfe4120161f28
alekseykorotenko <alekor86@gmail.com>
2023-10-16 19:29:42 +0300

    another way to get avarage num

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-let firstNumber = Number(prompt('Enter first number'));
-let secondNumber = Number(prompt('Enter second number'));
-let thirdNumber = Number(prompt('Enter third number'));
+// let firstNumber = Number(prompt('Enter first number'));
+// let secondNumber = Number(prompt('Enter second number'));
+// let thirdNumber = Number(prompt('Enter third number'));
 
-let avgNumber = (firstNumber + secondNumber + thirdNumber) / 3;
+let sumNumber = 0;
+
+for (let i = 0; i < 3; i++) {
+  let userNum = Number(prompt('Enter first number'));
+  sumNumber += userNum;
+}
+
+avgNumber = sumNumber / 3;
+
+// let avgNumber = (firstNumber + secondNumber + thirdNumber) / 3;
 
 alert(avgNumber);
