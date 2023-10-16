ca578b3bc8c310460cb07a038f6baadfad8e4c41
alekseykorotenko <alekor86@gmail.com>
2023-10-16 19:18:01 +0300

    js

---
+++ main.js
@@ -0,0 +1,7 @@
+let firstNumber = Number(prompt('Enter first number'));
+let secondNumber = Number(prompt('Enter second number'));
+let thirdNumber = Number(prompt('Enter third number'));
+
+let avgNumber = (firstNumber + secondNumber + thirdNumber) / 3;
+
+alert(avgNumber);
